4e28d9fa340ef3be19f7f39ae985dcae6efc0d59
plescaevelyn <plescaevelyn@yahoo.com>
2022-11-24 09:40:35 +0200

    added lab 8

---
+++ Sem1/ST2/Laboratories/Lab 8/lab8.m
@@ -0,0 +1,30 @@
+clc; clear variables;
+
+%% Method I of plotting a circle
+Mp = 10^(1/10);
+
+radius = Mp/(Mp^2-1);
+center = [-Mp^2/(Mp^2-1) 0];
+
+figure,
+viscircles(center,radius); grid;
+%% Method II of plotting a circle
+MdB = 2; % modulul in decibeli
+
+figure,
+cm(MdB); 
+%% 
+H = 9e4*tf(1,[1 135 0]);
+
+figure,
+nyquist(H); % open-loop Nyquist diagram
+hold on;
+cm(4); grid; shg; 
+axis([-3 0 -2 3])
+%%
+MdB = 7.2
+figure,
+nyquist(H); % open-loop Nyquist diagram
+hold on;
+cm(MdB); grid; shg; 
+axis([-3 0 -2 3])
