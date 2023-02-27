2f64a75f5c6fa60c9c69d0bde8b3b97a2381f96e
plescaevelyn <plescaevelyn@yahoo.com>
2023-02-27 11:16:41 +0200

    fully implemented the second exercise

--- Sem 2/RTS/src/main/java/Lab1/Exercise2.java
+++ Sem 2/RTS/src/main/java/Lab1/Exercise2.java
@@ -8,28 +8,72 @@ Calculate the sum and the product of the two matrices. Check the result.
  */
 
 public class Exercise2 {
-    int[][] sumMatrix = new int[3][3];
-    int[][] productMatrix = new int[3][3];
+    public static void main(String[] args) {
+        int[][] sumMatrix;
+        int[][] productMatrix;
 
-    int[][] firstMatrix = {
-        {2, 3, 1},
-        {7, 1, 6},
-        {9, 2, 4},
-    };
+        int[][] firstMatrix = {
+                {2, 3, 1},
+                {7, 1, 6},
+                {9, 2, 4},
+        };
 
-    int[][] secondMatrix = {
-            {8, 5, 3},
-            {3, 9, 2},
-            {2, 7, 3},
-    };
+        int[][] secondMatrix = {
+                {8, 5, 3},
+                {3, 9, 2},
+                {2, 7, 3},
+        };
 
-    // calculating and displaying the sum of the matrices
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            sumMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+        System.out.println("The two initial matrices are: ");
+        display3DMatrix(firstMatrix);
+        System.out.println("\n");
+        display3DMatrix(secondMatrix);
+
+        sumMatrix = sumOfTheMatrices(firstMatrix, secondMatrix);
+        productMatrix = productOfTheMatrices(firstMatrix, secondMatrix);
+    }
+
+    // function for displaying a three-dimensional matrix
+    public static void display3DMatrix(int[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println("\n");
         }
     }
 
-    // calculating and displaying the product of the matrices
+    // calculating and displaying the sum of the matrices
+    public static int[][] sumOfTheMatrices(int[][] firstMatrix, int[][] secondMatrix) {
+        int[][] sumMatrix = new int[3][3];
 
+        System.out.println("The sum of the two matrices is equal with: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sumMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
+
+        display3DMatrix(sumMatrix);
+
+        return sumMatrix;
+    }
+
+    // calculating and displaying the product of the matrices
+    public static int[][] productOfTheMatrices(int[][] firstMatrix, int[][] secondMatrix) {
+        int[][] productMatrix = new int[3][3];
+
+        System.out.println("The product of the two matrices is equal with: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    productMatrix[i][j] = firstMatrix[i][k] * secondMatrix[k][j];
+                }
+            }
+        }
+
+        display3DMatrix(productMatrix);
+
+        return productMatrix;
+    }
 }
