c75c5ff4745aef59a26d7195aaa044c088fef6bf
plescaevelyn <plescaevelyn@yahoo.com>
2023-03-01 14:24:45 +0200

    Implemented the examples from Laboratory 1

---
+++ Sem 2/DT/Lab 1/1.3.1/app.js
@@ -0,0 +1,50 @@
+console.log("Welcome  to data  transmission");
+document.getElementById("message").innerHTML = "Message from  JavaScript";
+
+var sum = 10;
+var  name = "Alexandru";
+var  isActive = true;
+var  user = {id: 1, name:"Andrei", age: 21, };
+
+var  user = {
+  "id": 1,
+  "name": "Alexandru  Cristea",
+  "username": "acristea",
+  "email": "acristea@test.com",
+  "address": {
+    "street": "Padin",
+    "number": "Ap. 10",
+    "city": "Cluj -Napoca",
+    "zipcode": "123456",
+    "geo": {
+      "lat": "46.783364",
+      "lng": "23.546472"}
+    },
+    "phone": "004-07xx -123456",
+    "company": {
+      "name": "XYZ",
+      "domain": "Air  Traffic  Management",
+      "cities": ["Cluj -Napoca", "Vienna", "Paris"]
+    }
+  }
+
+console.log(user.name);
+console.log(user.address.geo.lat);
+console.log(user.company.name);
+console.dir(user.company.cities);
+consoler.log(user.company.cities[0]);
+
+function print(message) {
+  console.log(message);
+}
+
+print("hello")
+
+var password = "123456";
+consoler.log(password=="123456"?"ALLOW":"DENY");
+
+if (password == "123456") {
+  console.log("permission accepted");
+} else {
+  console.log("permission denied");
+}
