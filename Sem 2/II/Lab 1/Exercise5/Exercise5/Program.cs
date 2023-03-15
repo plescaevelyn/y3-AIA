2f6ab07060297159c541c3fb6b9963f06fff4ef0
plescaevelyn <plescaevelyn@yahoo.com>
2023-03-15 14:13:03 +0200

    solved all exercises from lab 1

---
+++ Sem 2/II/Lab 1/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Sa se introduca un sir de numere intregi de la tastatura si sa se calculeze media geometrica si aritmetica a numerelor */
+
+using System;
+
+namespace Exercise5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int product = 1;
+
+            Console.WriteLine("Introdu lungimea sirului de numere:\t");
+            int sequenceLength = int.Parse(Console.ReadLine());
+            int[] sequence = new int[sequenceLength];
+
+            Console.WriteLine("Introdu sirul de numere:\t");
+            for (int i = 0; i < sequenceLength; i++)
+            {
+                sequence[i] = int.Parse(Console.ReadLine());
+                sum += sequence[i];
+                product *= sequence[i];
+            }
+
+            Console.WriteLine("Media aritmetica a numerelor este:\t{0}\n" +
+                "Media geometrica a numerelor este:\t{1}\n",
+                sum / sequenceLength, Math.Sqrt(product));
+        }
+    }
+}
