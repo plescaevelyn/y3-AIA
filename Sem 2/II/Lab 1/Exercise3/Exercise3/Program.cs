000081208d2f6c2a818d40025a87129464ee66c3
plescaevelyn <plescaevelyn@yahoo.com>
2023-03-15 13:22:15 +0200

    solved exercise 3

---
+++ Sem 2/II/Lab 1/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Exercise3 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your initial temperature in: Celsius or Fahrenheit? [C/F]");
+            char initialTemperature = Console.ReadLine()[0];
+
+            float temperatureC, temperatureF;
+
+            Console.WriteLine("Give the temperature.\t");
+            switch(initialTemperature)
+            {
+                case 'C':
+                    temperatureC = int.Parse(Console.ReadLine());   
+                    temperatureF = TemperatureConverter.convertFromCelsiusToFahrenheit(temperatureC);
+                    TemperatureConverter temperatureConverter = new TemperatureConverter(initialTemperature, temperatureC, temperatureF);
+                    temperatureConverter.displayTemperatures(temperatureC, temperatureF);
+                    break;
+                case 'F':
+                    temperatureF = int.Parse(Console.ReadLine());
+                    temperatureC = TemperatureConverter.convertFromFahrenheitToCelsius(temperatureF);
+                    TemperatureConverter temperatureConverter1 = new TemperatureConverter(initialTemperature, temperatureC, temperatureF);
+                    temperatureConverter1.displayTemperatures(temperatureC, temperatureF);
+                    break;
+            }
+        }
+    }
+}
---
+++ Sem 2/II/Lab 1/Exercise3/Exercise3/TemperatureConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class TemperatureConverter
+    {
+        float temperatureInCelsius;
+        float temperatureInFahrenheit;
+        char initialTempGivenIn;
+
+        public TemperatureConverter(char initialTempGivenIn, float temperatureInCelsius, float temperatureInFahrenheit) {
+            this.initialTempGivenIn = initialTempGivenIn;
+            this.temperatureInCelsius = temperatureInCelsius;
+            this.temperatureInFahrenheit = convertFromCelsiusToFahrenheit(temperatureInCelsius);
+        }
+
+        public static float convertFromCelsiusToFahrenheit(float temperatureInCelsius)
+        {
+            return temperatureInCelsius * 9 / 5 + 32;
+        }
+        public static float convertFromFahrenheitToCelsius(float temperatureInFahrenheit) { 
+            return (temperatureInFahrenheit - 32) * 5 / 9;
+        }
+
+        public void displayTemperatures(float temperatureInCelsius, float temperatureInFahrenheit)
+        {
+            Console.WriteLine("Today's temperatures are:\n\t{0}°C\t\t\t{1}°F", temperatureInCelsius, temperatureInFahrenheit);
+        }
+    }
+}
