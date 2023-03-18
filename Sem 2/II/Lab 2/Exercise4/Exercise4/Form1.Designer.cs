44f34aaa018558fcb4daf659af5311b8ee470566
plescaevelyn <plescaevelyn@yahoo.com>
2023-03-18 17:49:22 +0200

    implemented the first tab

--- Sem 2/II/Lab 2/Exercise4/Exercise4/Form1.Designer.cs
+++ Sem 2/II/Lab 2/Exercise4/Exercise4/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            panel1 = new Panel();
+            listBox1 = new ListBox();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(904, 521);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(panel1);
+            tabPage1.Controls.Add(listBox1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(896, 488);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(247, 21);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(624, 464);
+            panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(24, 21);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(194, 464);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(571, 227);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(917, 565);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private ListBox listBox1;
+        private TabPage tabPage2;
+        private Panel panel1;
     }
 }
--- Sem 2/II/Lab 2/Exercise4/Exercise4/Form1.cs
+++ Sem 2/II/Lab 2/Exercise4/Exercise4/Form1.cs
@@ -5,6 +5,48 @@ namespace Exercise4
         public Form1()
         {
             InitializeComponent();
+            listBox1.Items.Add("Crasna Moftinu Mic");
+            listBox1.Items.Add("Gloria Moftinu Mare");
+            listBox1.Items.Add("Recolta Sanislau");
+            listBox1.Items.Add("Vulturii Santau");
+            listBox1.Items.Add("Cetate Ardud");
+            listBox1.Items.Add("Real Andrid");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedItem)
+            {
+                case "Crasna Moftinu Mic":
+                    Image image =
+                    panel1.BackgroundImage = Image.FromFile("E:\\y3AIA\\Sem 2\\II\\Lab 2\\Exercise4\\Exercise4\\footbal_team_logos\\moftinu_mic.jpg");
+                    break;
+                case "Gloria Moftinu Mare":
+                    panel1.BackgroundImage = Image.FromFile("E:\\y3AIA\\Sem 2\\II\\Lab 2\\Exercise4\\Exercise4\\footbal_team_logos\\moftinu_mare.jpg");
+                    break;
+                case "Recolta Sanislau":
+                    panel1.BackgroundImage = Image.FromFile("E:\\y3AIA\\Sem 2\\II\\Lab 2\\Exercise4\\Exercise4\\footbal_team_logos\\sanislau.png");
+                    break;
+                case "Vulturii Santau":
+                    panel1.BackgroundImage = Image.FromFile("E:\\y3AIA\\Sem 2\\II\\Lab 2\\Exercise4\\Exercise4\\footbal_team_logos\\santau.png");
+                    break;
+                case "Cetate Ardud":
+                    panel1.BackgroundImage = Image.FromFile("E:\\y3AIA\\Sem 2\\II\\Lab 2\\Exercise4\\Exercise4\\footbal_team_logos\\ardud.jpg");
+                    break;
+                case "Real Andrid":
+                    panel1.BackgroundImage = Image.FromFile("E:\\y3AIA\\Sem 2\\II\\Lab 2\\Exercise4\\Exercise4\\footbal_team_logos\\andrid.jpg");
+                    break;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
