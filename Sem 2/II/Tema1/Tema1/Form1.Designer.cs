eda1e2df680d384750e9c7f6673b2e0bbad43c50
plescaevelyn <plescaevelyn@yahoo.com>
2023-03-28 00:59:05 +0300

    implemented the homework

--- Sem 2/II/Tema1/Tema1/Form1.Designer.cs
+++ Sem 2/II/Tema1/Tema1/Form1.Designer.cs
@@ -29,189 +29,260 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            numericUpDown1 = new NumericUpDown();
-            label5 = new Label();
-            button1 = new Button();
-            radioButton1 = new RadioButton();
-            radioButton2 = new RadioButton();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            listBox1 = new ListBox();
-            label6 = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
             panel1 = new Panel();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            label6 = new Label();
+            listBox1 = new ListBox();
+            textBoxFirstName = new TextBox();
+            textBoxLastName = new TextBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button1 = new Button();
+            label5 = new Label();
+            yrsOfExp = new NumericUpDown();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            listBox2 = new ListBox();
+            label7 = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)yrsOfExp).BeginInit();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // tabControl1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(48, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(185, 38);
-            label1.TabIndex = 0;
-            label1.Text = "Application";
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(7, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(869, 511);
+            tabControl1.TabIndex = 0;
             // 
-            // label2
+            // tabPage1
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(48, 69);
-            label2.Name = "label2";
-            label2.Size = new Size(79, 20);
-            label2.TabIndex = 1;
-            label2.Text = "Last name:";
+            tabPage1.Controls.Add(panel1);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(listBox1);
+            tabPage1.Controls.Add(textBoxFirstName);
+            tabPage1.Controls.Add(textBoxLastName);
+            tabPage1.Controls.Add(radioButton2);
+            tabPage1.Controls.Add(radioButton1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(yrsOfExp);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(861, 478);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // panel1
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(48, 118);
-            label3.Name = "label3";
-            label3.Size = new Size(80, 20);
-            label3.TabIndex = 2;
-            label3.Text = "First name:";
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Location = new Point(475, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(386, 479);
+            panel1.TabIndex = 27;
             // 
-            // label4
+            // label6
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(48, 162);
-            label4.Name = "label4";
-            label4.Size = new Size(60, 20);
-            label4.TabIndex = 3;
-            label4.Text = "Gender:";
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(220, 211);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(58, 27);
-            numericUpDown1.TabIndex = 4;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(48, 211);
-            label5.Name = "label5";
-            label5.Size = new Size(140, 20);
-            label5.TabIndex = 5;
-            label5.Text = "Years of experience:";
-            // 
-            // button1
-            // 
-            button1.Location = new Point(166, 399);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 6;
-            button1.Text = "Submit!";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // radioButton1
-            // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(153, 162);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(63, 24);
-            radioButton1.TabIndex = 7;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "male";
-            radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(246, 162);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(76, 24);
-            radioButton2.TabIndex = 8;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "female";
-            radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(143, 67);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(212, 27);
-            textBox1.TabIndex = 9;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(143, 118);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(212, 27);
-            textBox2.TabIndex = 10;
+            label6.AutoSize = true;
+            label6.Location = new Point(59, 277);
+            label6.Name = "label6";
+            label6.Size = new Size(212, 20);
+            label6.TabIndex = 26;
+            label6.Text = "The role you want to apply for:";
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 20;
-            listBox1.Location = new Point(278, 267);
+            listBox1.Location = new Point(289, 277);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(150, 104);
-            listBox1.TabIndex = 11;
+            listBox1.Size = new Size(180, 104);
+            listBox1.TabIndex = 25;
             // 
-            // label6
+            // textBoxFirstName
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(48, 267);
-            label6.Name = "label6";
-            label6.Size = new Size(212, 20);
-            label6.TabIndex = 12;
-            label6.Text = "The role you want to apply for:";
+            textBoxFirstName.Location = new Point(154, 128);
+            textBoxFirstName.Name = "textBoxFirstName";
+            textBoxFirstName.Size = new Size(212, 27);
+            textBoxFirstName.TabIndex = 24;
             // 
-            // panel1
+            // textBoxLastName
             // 
-            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
-            panel1.Location = new Point(446, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(354, 455);
-            panel1.TabIndex = 13;
+            textBoxLastName.Location = new Point(154, 77);
+            textBoxLastName.Name = "textBoxLastName";
+            textBoxLastName.Size = new Size(212, 27);
+            textBoxLastName.TabIndex = 23;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(257, 172);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(76, 24);
+            radioButton2.TabIndex = 22;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(164, 172);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 24);
+            radioButton1.TabIndex = 21;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(177, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 20;
+            button1.Text = "Submit!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(59, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(140, 20);
+            label5.TabIndex = 19;
+            label5.Text = "Years of experience:";
+            // 
+            // yrsOfExp
+            // 
+            yrsOfExp.Location = new Point(231, 221);
+            yrsOfExp.Name = "yrsOfExp";
+            yrsOfExp.Size = new Size(58, 27);
+            yrsOfExp.TabIndex = 18;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(59, 172);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 20);
+            label4.TabIndex = 17;
+            label4.Text = "Gender:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(59, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 20);
+            label3.TabIndex = 16;
+            label3.Text = "First name:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(59, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 15;
+            label2.Text = "Last name:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(59, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(185, 38);
+            label1.TabIndex = 14;
+            label1.Text = "Application";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(listBox2);
+            tabPage2.Controls.Add(label7);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(861, 478);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 20;
+            listBox2.Location = new Point(223, 77);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(430, 364);
+            listBox2.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AccessibleRole = AccessibleRole.None;
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(17, 16);
+            label7.Name = "label7";
+            label7.Size = new Size(196, 32);
+            label7.TabIndex = 0;
+            label7.Text = "Applicants list:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(panel1);
-            Controls.Add(label6);
-            Controls.Add(listBox1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(radioButton2);
-            Controls.Add(radioButton1);
-            Controls.Add(button1);
-            Controls.Add(label5);
-            Controls.Add(numericUpDown1);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            ClientSize = new Size(888, 525);
+            Controls.Add(tabControl1);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)yrsOfExp).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private NumericUpDown numericUpDown1;
-        private Label label5;
-        private Button button1;
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private ListBox listBox1;
-        private Label label6;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
         private Panel panel1;
+        private Label label6;
+        private ListBox listBox1;
+        private TextBox textBoxFirstName;
+        private TextBox textBoxLastName;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private Label label5;
+        private NumericUpDown yrsOfExp;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TabPage tabPage2;
+        private ListBox listBox2;
+        private Label label7;
     }
 }
--- Sem 2/II/Tema1/Tema1/Form1.cs
+++ Sem 2/II/Tema1/Tema1/Form1.cs
@@ -1,7 +1,14 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Security.Cryptography;
+
 namespace Tema1
 {
     public partial class Form1 : Form
     {
+        SqlConnection myCon = new SqlConnection();
+        DataSet dsApplicants;
         public Form1()
         {
             InitializeComponent();
@@ -10,12 +17,64 @@ namespace Tema1
             listBox1.Items.Add("Software Tester Intern");
             listBox1.Items.Add("DevOps Intern");
             listBox1.Items.Add("Systems Admin Intern");
+
+            myCon.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\y3AIA\\Sem 2\\II\\Tema1\\Tema1\\Database1.mdf;Integrated Security=True";
+            myCon.Open();
+
+            dsApplicants = new DataSet();
+            SqlDataAdapter daApplicants = new SqlDataAdapter("SELECT * FROM Applicants", myCon);
+            daApplicants.Fill(dsApplicants, "Applicants");
+
+            foreach (DataRow dr in dsApplicants.Tables["Applicants"].Rows)
+            {
+                String name = dr.ItemArray.GetValue(1).ToString() + " " + dr.ItemArray.GetValue(2).ToString();
+                listBox2.Items.Add(name);
+            }
+
+            myCon.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
+            myCon.Open();
+
+            string firstName = textBoxFirstName.Text;
+            string lastName = textBoxLastName.Text;
+            string gender = radioButton1.Checked ? "m" : "f";
+            string yearsOfExperience = yrsOfExp.Value.ToString();
+
+            if (listBox1.SelectedItem == null || firstName == "" || lastName == "")
+            {
+                MessageBox.Show("Fill in all the data!");
+                myCon.Close();
+
+                return;
+            }
+
+            string selectedProgram = listBox1.SelectedItem.ToString();
+
+            string insertQuery = "INSERT INTO Universitati (LastName, FirstName, Gender, YrsOfExp, Role) " +
+                "VALUES (@firstName, @lastName, @gender, @yearOfExperience, @selectedProgram)";
+
+            SqlCommand cmd = new SqlCommand(insertQuery, myCon);
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+            }
+
+            myCon.Close();
+            MessageBox.Show("Applicant registered!");
+
+            dsApplicants.Clear();
+            SqlDataAdapter daApplicants = new SqlDataAdapter("SELECT * FROM Applicants", myCon);
+            daApplicants.Fill(dsApplicants, "Applicants");
+
+            string numeApp = textBoxFirstName.Text + " " + textBoxLastName.Text;
+            listBox2.Items.Add(numeApp);
         }
     }
 }
