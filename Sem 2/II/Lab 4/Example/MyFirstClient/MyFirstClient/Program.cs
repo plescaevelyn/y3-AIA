55dec913edc44359667bd5c7d7db20df6d791254
plescaevelyn <evelynplesca@gmail.com>
2023-04-05 19:55:27 +0300

    added example

---
+++ Sem 2/II/Lab 4/Example/MyFirstClient/MyFirstClient/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFirstClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFirstClient.ServiceReference1.WebService1SoapClient service = new MyFirstClient.ServiceReference1.WebService1SoapClient();
+
+
+            // int sum = service.Add(2, 3);
+            // Console.WriteLine("The sum is " + sum.ToString());
+            // Console.ReadKey();
+        }
+    }
+}
+    
