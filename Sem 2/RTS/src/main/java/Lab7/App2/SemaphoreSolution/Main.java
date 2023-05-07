26f3487939c18d199c3d1a6cdc69fccc92dbad31
plescaevelyn <evelynplesca@gmail.com>
2023-05-07 20:10:32 +0300

    added solution for lab 7 app 2

---
+++ Sem 2/RTS/src/main/java/Lab7/App2/SemaphoreSolution/Main.java
@@ -0,0 +1,16 @@
+package Lab7.App2.SemaphoreSolution;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+public class Main {
+    public static void main(String[] args) {
+        Semaphore semaphore1 = new Semaphore(1);
+        Semaphore semaphore2 = new Semaphore(1);
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+
+        new ExecutionThread(semaphore1, 2, 4, 4, cyclicBarrier).start();
+        new ExecutionThread(semaphore2, 3, 6, 3, cyclicBarrier).start();
+        new ExecutionThread(semaphore2, 2, 5, 5, cyclicBarrier).start();
+    }
+}
