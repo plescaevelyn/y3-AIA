d54ac085484f2515449c3ac04d18dbaf20bd32ad
plescaevelyn <evelynplesca@gmail.com>
2023-05-07 19:16:41 +0300

    fixed bug

--- Sem 2/RTS/src/main/java/Lab7/App1/SemaphoreSolution/Main.java
+++ Sem 2/RTS/src/main/java/Lab7/App1/SemaphoreSolution/Main.java
@@ -1,7 +1,5 @@
 package Lab7.App1.SemaphoreSolution;
 
-import Lab7.App1.ReentrantLockSolution.ExecutionThread;
-
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 
@@ -11,7 +9,7 @@ public class Main {
         Semaphore semaphore2 = new Semaphore(1);
         CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
 
-        new Lab7.App1.ReentrantLockSolution.ExecutionThread(semaphore1, semaphore2, 2, 4, 4, 4, 6, cyclicBarrier).start();
+        new ExecutionThread(semaphore1, semaphore2, 2, 4, 4, 4, 6, cyclicBarrier).start();
         new ExecutionThread(semaphore2, semaphore1, 3, 5, 5, 5, 7, cyclicBarrier).start();
     }
 }
