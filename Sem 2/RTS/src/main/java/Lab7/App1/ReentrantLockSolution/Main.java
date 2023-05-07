3b6fb12ff4a488025151e9a7e56a5853e6e0fbdd
plescaevelyn <evelynplesca@gmail.com>
2023-05-07 19:18:42 +0300

    fixed bug

--- Sem 2/RTS/src/main/java/Lab7/App1/ReentrantLockSolution/Main.java
+++ Sem 2/RTS/src/main/java/Lab7/App1/ReentrantLockSolution/Main.java
@@ -5,16 +5,11 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) {
-        ReentrantLock reentrantLock1 = new ReentrantLock();
-        ReentrantLock reentrantLock2 = new ReentrantLock();
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+        ReentrantLock semaphore1 = new ReentrantLock();
+        ReentrantLock semaphore2 = new ReentrantLock();
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
 
-        ExecutionThread thread1 = new ExecutionThread(reentrantLock1, reentrantLock2, 2, 4, 4, 4, 6, cyclicBarrier);
-        ExecutionThread thread2 = new ExecutionThread(reentrantLock1, reentrantLock2, 3, 5, 5, 5, 7, cyclicBarrier);
-        ExecutionThread thread3 = new ExecutionThread(reentrantLock1, reentrantLock2, 1, 3, 3, 3, 5, cyclicBarrier);
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
+        new ExecutionThread(semaphore1, semaphore2, 2, 4, 4, 4, 6, cyclicBarrier).start();
+        new ExecutionThread(semaphore2, semaphore1, 3, 5, 5, 5, 7, cyclicBarrier).start();
     }
 }
